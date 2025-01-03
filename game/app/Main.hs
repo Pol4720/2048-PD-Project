d58649d62b03c0638f86c76cb795bb277b56e04d
Pol472 <128186456+Pol472@users.noreply.github.com>
2025-01-03 18:14:58 -0500

    Add random number generation and update dependencies for game logic

--- game/app/Main.hs
+++ game/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import System.Random (randomRIO) -- Importar random
 import Game.Game -- Import the main game logic
 
 -- Define the initial state of the game
