3e7663b0df9c20636be4b1f383a8fc74c71f62aa
LoLProM <mauriciosundejimenez@gmail.com>
2025-02-04 12:50:59 -0500

    Adding new graphics and refactoring

--- game/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-
-# Changelog for 2048-PD-Project
-
-## Unreleased changes
-
-- Initial release of the 2048 game in Haskell.
--- game2048/app/Main.hs
+++ game2048/app/Main.hs
@@ -1,16 +1,63 @@
-module Main (main) where
+module Main (main)
+where
 
 import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
-import Lib (initialState, render, handleInput, update)
+import Graphics.Gloss.Data.Color
+import Graphics.Gloss.Interface.IO.Game
+import Data.List (transpose,reverse)
+import Data.List.Split (chunksOf)
+import System.Random (StdGen,getStdGen,random,randoms)
+import Control.Lens (set,element)
 
 -- Definir la función principal para ejecutar el juego
-main :: IO ()
-main = play
-    (InWindow "2048 Game" (800, 600) (100, 100)) -- Configuración de la ventana
-    white                                        -- Color de fondo
-    60                                           -- Fotogramas por segundo
-    initialState                                 -- Estado inicial
-    render                                       -- Función de renderizado
-    handleInput                                  -- Función de manejo de entrada
-    update                                       -- Función de actualización
+main =
+  do
+    g <- getStdGen
+    play
+      (InWindow "2048 in Haskell by Richard & Mauricio"
+         (410,500)
+         (20,20)
+      )
+      (makeColor 193 177 156 255) -- background color
+      fps -- simulation steps per second
+      (initPositions g) -- initial world
+      drawWorld -- function to convert world to picture
+      handleInputEvents -- TODO add randomness
+      stepWorld
+
+fps = 20
+
+data Tile = Tile { val :: Int, popInTime :: Float, popOutTime :: Float} deriving (Eq,Show)
+type Row = [Tile]
+data World = World {board :: [[Tile]], gen :: StdGen, score :: Int }
+
+instance Eq World where
+    x == y = board x == board y && score x == score y
+
+tileIntFunction :: (Int -> Int) -> Tile -> Tile
+tileIntFunction f t = t {val=f (val t)}
+
+setVal :: Tile -> Int -> Tile
+setVal t i = t {val=i}
+
+setVals :: [Tile] -> [Int] -> [Tile]
+setVals ts is = zipWith setVal ts is
+
+setValss :: [[Tile]] -> [[Int]] -> [[Tile]]
+setValss tss iss = zipWith setVals tss iss
+
+---------------------------------------------
+-- Construccion
+---------------------------------------------
+
+toInt :: Bool -> Int
+toInt True = 1
+toInt False = 0
+
+makeTile :: Int -> Tile
+makeTile i = Tile {val=i, popInTime=0.0, popOutTime=0.0}
+
+-- Comienza con dos 2s
+initPositions :: StdGen -> World
+initPositions g = let origBoard = chunksOf 4 $ map makeTile $ replicate 16 0
+                    in addTile $ addTile World {board=origBoard,gen=g,score=0 }
